7ba925909ad65ec4ec9bcc79e4b24a1e0c04f8fa
rkumar51 <rkumar51@DESKTOP-AP2L98D.auth.hpicorp.net>
2021-05-13 14:13:43 +0530

    just one change

--- Test1_Using_Java6/src/com/rajeev/test/HelloWorld.java
+++ Test1_Using_Java6/src/com/rajeev/test/HelloWorld.java
@@ -2,10 +2,12 @@ package com.rajeev.test;
 
 public class HelloWorld {
 
+	int i = 0;
 	
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("This is my first Program");
+		System.out.println(i);
 
 	}
 
